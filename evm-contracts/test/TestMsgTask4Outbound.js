4c037cbc62514d6e49e10e0ffdae154c3b4cbd64
Orel <38098634+Orel-bytes@users.noreply.github.com>
2025-12-29 15:29:49 +0800

    update evm contracts for message task

---
+++ evm-contracts/test/TestMsgTask4Outbound.js
@@ -0,0 +1,73 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const Config = require("../hardhat.config");
+const Deployed_WanChain = require("../deployed/wanTestnet.json");
+const Deployed_Cardano = require("../deployed/cardanoPreprod.json");
+const GXTokenSc = require("../deployed/scAbi/XToken.json");
+const TokenHomeSc = require("../deployed/scAbi/ERC20TokenHome4CardanoV2.json");
+
+const EVM_GXTOKEN_SCADDRESS = Deployed_WanChain.XToken; 
+const EVM_TOKENHOME_SCADDRESS = Deployed_WanChain.TokenHome; 
+const WAITTING_SECONDS = 30 * 1000;
+
+
+function sleep(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve();
+    }, time);
+  })
+}
+
+describe("\n\n****Test ERC20TokenHome4Cardano", function () {
+  let tokenHome4CardanoSc, tokenHome4CardanoScInst, gxTokenSc, gxTokenScInst, nodeUrl, tokenHomeScAbi, gxTokenScAbi, rpcProvider, owner, testAccount;
+
+
+  before(async () => {
+
+    [owner, testAccount] = await ethers.getSigners();
+    console.log("owner:", owner.address);
+    console.log("testAccount:", testAccount.address);
+
+    nodeUrl = Config.networks.wanchainTestnet.url;
+    tokenHomeScAbi = TokenHomeSc.abi;
+    gxTokenScAbi = GXTokenSc.abi;
+    rpcProvider = new ethers.providers.JsonRpcProvider(this.nodeUrl);
+
+    // to create tokenHome sc instance
+    tokenHome4CardanoSc = new ethers.Contract(EVM_TOKENHOME_SCADDRESS, tokenHomeScAbi, rpcProvider);
+    tokenHome4CardanoScInst = tokenHome4CardanoSc.connect(testAccount); //owner
+
+    // to create token sc instance
+    gxTokenSc = new ethers.Contract(EVM_GXTOKEN_SCADDRESS, gxTokenScAbi, rpcProvider);
+    gxTokenScInst = gxTokenSc.connect(testAccount); //owner
+
+  });
+
+
+  describe("\n\n===>To Cross Token from Wan to Cardano", function () {
+    it("To decode plutusData by calling TokenHome's send function", async function () {
+
+      const addr1BalanceBefore = await gxTokenScInst.balanceOf(testAccount.address);
+      console.log("balance of test account:", addr1BalanceBefore);
+
+      const approveAmount = 10000000;
+      await gxTokenScInst.approve(EVM_TOKENHOME_SCADDRESS, approveAmount);
+      console.log("to approve token for tokenHome sc..");
+      await sleep(WAITTING_SECONDS);
+
+      const plutusDataMsg = "0xd8799fd87a9fd8799fd8799f581c76f045bbc3f00ae7bea379b7b501154d4941a9d4acf765f525961e1affd8799fd8799fd8799f581c76f045bbc3f00ae7bea379b7b501154d4941a9d4acf765f525961e1affffffffff192710ff";
+      await tokenHome4CardanoScInst.send(
+        plutusDataMsg
+      );
+
+      await sleep(WAITTING_SECONDS);
+      const addr1BalanceAfter = await gxTokenScInst.balanceOf(testAccount.address);
+      console.log("balance of test account:", addr1BalanceAfter);
+
+    });
+  });
+
+
+});
